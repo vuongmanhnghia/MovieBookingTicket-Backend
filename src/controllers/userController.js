fad0be2578eb62ffc47e7f1343c49598faee3a4e
Vuong Manh Nghia <vuongmanhnghia@gmail.com>
2024-06-04 01:22:05 +0700

    Gọi API lấy dữ liệu users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,6 +18,26 @@ let handleLogin = async (req, res) => {
 		user: userData.user ? userData.user : {},
 	});
 };
+
+let handleGetAllUsers = async (req, res) => {
+	let id = req.query.id; //all, id
+	if (!id) {
+		return res.status(200).json({
+			errCode: 1,
+			errMessage: "Missing required parameter! Please check again!",
+			users: [],
+		});
+	}
+	let users = await userService.getAllUsers(id);
+	console.log(users);
+	return res.status(200).json({
+		errCode: 0,
+		errMessage: "Ok",
+		users,
+	});
+};
+
 module.exports = {
 	handleLogin: handleLogin,
+	handleGetAllUsers: handleGetAllUsers,
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -59,6 +59,34 @@ let checkUserEmail = (email) => {
 	});
 };
 
+let getAllUsers = (userId) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let users = "";
+			if (userId === "ALL") {
+				users = await db.User.findAll({
+					attributes: {
+						exclude: ["password"],
+					},
+				});
+			}
+			if (userId && userId !== "ALL") {
+				users = await db.User.findOne({
+					where: { id: userId },
+					attributes: {
+						exclude: ["password"],
+					},
+				});
+			}
+			console.log(users);
+			resolve(users);
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
 module.exports = {
 	handleUserLogin: handleUserLogin,
+	getAllUsers: getAllUsers,
 };
