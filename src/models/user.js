ca4ae34f61f49c6927dc9061e5e25efbf6c30b1b
Vuong Manh Nghia <vuongmanhnghia@gmail.com>
2024-06-09 19:45:56 +0700

    sửa key thành keyMap-2

--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,11 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			// define association here
+			User.belongsTo(models.Allcode, {
+				foreignKey: "gender",
+				targetKey: "keyMap",
+				as: "genderData",
+			});
 		}
 	}
 	User.init(
